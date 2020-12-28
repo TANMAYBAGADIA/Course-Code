9b5b0806d9719f56161af976e24440e734c6cf1e
TANMAYBAGADIA <tanmaybagadia1301@gmail.com>
2020-12-28 21:07:47 +0530

    Adding Bootstrap
    Signed-off-by: TANMAYBAGADIA <tanmaybagadia1301@gmail.com>

---
+++ insertion-sort/script.js
@@ -0,0 +1,22 @@
+function insertionSort(arr) {
+        let memIndex = 0
+        for (let i = 0; i < arr.length; i++) {
+          memIndex = i;
+          for (let j = i + 1; j >= 0; --j) {
+            if (arr[j] >= arr[i]) {
+              break;
+            }
+            if (arr[j] < arr[i]) {
+              var temp = arr[i];
+              arr[i] = arr[j];
+              arr[j] = temp;
+              i = i - 1;
+            }
+          }
+          i = memIndex;
+        }
+        return arr;
+      }
+      const arr = [5, 1, 6, 2, 4, 9, 9, 3, 1, 1, 1];
+      console.log('Unsorted array', arr);
+      console.log('Sorted array:', insertionSort(arr));
